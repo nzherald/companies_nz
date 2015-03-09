9fca53c0135a4925106654f368794e913eaa2ce0
Caleb <caleb.tutty@nzherald.co.nz>
2015-03-09 20:21:58 +1300

    Implementing endpoints

--- lib/nz/companies.rb
+++ lib/nz/companies.rb
@@ -1,7 +1,10 @@
-require "nz/companies/version"
+require 'nz/companies/version'
 
 module NZ
   module Companies
-    # Your code goes here...
+    URL = 'http://www.businessdata.govt.nz'
+    TEST_URL = 'http://www.eat.business.govt.nz'
   end
 end
+
+%w[client hmac_signer http_client].each { |f| require "nz/companies/#{f}" }
